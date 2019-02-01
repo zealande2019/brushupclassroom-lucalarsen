ce0a0fedce06a6ca037f614b1ad43f3dd07e4e72
lucalarsen <lucalarsen@hotmail.com>
2019-02-01 15:04:22 +0100

    class studerende lavet

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        public string Navn { get; set; }
+        public int fødselsmåned { get; set; }
+        public int fødselsdag { get; set; }
+
+        public Studerende(string Navn, int fødselsmåned, int fødselsdag)
+        {
+
+        }
+    }
+}
