db02767da26e8e53a232758a2946aae86913abfc
lucalarsen <lucalarsen@hotmail.com>
2019-02-01 15:12:16 +0100

    class klasserum lavet

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        public string KlasseNavn { get; set; }
+
+        List<Studerende> Klasseliste = new List<Studerende>();
+
+        public DateTime SemesterStart { get; set; }
+
+        public KlasseRum()
+        {
+
+        }
+    }
+}
